b0d6bb09032fc96b6722087128142c3f1412d834
Andreas Untergasser <andreas@untergasser.de>
2017-07-12 14:46:02 +0000

    Added the contact information

--- html/wilyDNAEditor.js
+++ html/wilyDNAEditor.js
@@ -34,7 +34,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Set here the Version
-var wdeVVersion = "0.7.4";
+var wdeVVersion = "0.7.5";
 
 // Display Variables
 var prevTabPage = "WDE_main_tab";
