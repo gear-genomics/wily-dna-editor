7d6efc083f1fbad865ef33ebe97e18d1ee786b65
Andreas Untergasser <andreas@untergasser.de>
2017-09-14 18:47:34 +0200

    Test Git commit...

--- html/wilyDNAEditor.js
+++ html/wilyDNAEditor.js
@@ -34,7 +34,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Set here the Version
-var wdeVVersion = "0.9.10";
+var wdeVVersion = "0.9.11";
 
 // Display Variables
 var prevTabPage = "WDE_main_tab";
